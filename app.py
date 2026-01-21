9cb5b869a0427b6865114ef9a98c886e00de225b
maj-alero <amajuogbe@gmail.com>
2026-01-21 11:42:45 +0100

    Made changes

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict():
                                neighborhoods=sorted(['CollgCr', 'Veenker', 'Crawfor', 'NoRidge']))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
